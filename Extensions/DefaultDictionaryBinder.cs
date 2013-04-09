0ccc72321672043bfba92d0b913e018b940c9665
davemorgantexas <davemorgantexas@gmail.com>
2013-04-09 13:54:50 -0500

    Collection class support, performance enhancements
    Support for reading collection classes with broken indexes.
    (e.g. a[1],a[3],a[12]...)
    Performance issue found when testing with collection classes nested three deep. 
    (e.g. MovieTheater[0].Employee[1].Hours[2].Start)

--- Extensions/DefaultDictionaryBinder.cs
+++ Extensions/DefaultDictionaryBinder.cs
@@ -31,12 +31,18 @@ using System.ComponentModel;
         private IEnumerable<string> GetValueProviderKeys(ControllerContext context)
         {
 #if !ASPNETMVC1
-            List<string> keys = new List<string>();
-            keys.AddRange(context.HttpContext.Request.Form.Keys.Cast<string>());
-            keys.AddRange(((IDictionary<string, object>)context.RouteData.Values).Keys.Cast<string>());
-            keys.AddRange(context.HttpContext.Request.QueryString.Keys.Cast<string>());
-            keys.AddRange(context.HttpContext.Request.Files.Keys.Cast<string>());
-            return keys;
+            IDictionary contextItems = HttpContext.Current.Items;
+            // Do not reference this key elsewhere, this is strictly for cache.
+            if (!contextItems.Contains("ValueProviderKeys"))
+            {
+                List<string> keys = new List<string>();
+                keys.AddRange(context.HttpContext.Request.Form.Keys.Cast<string>());
+                keys.AddRange(((IDictionary<string, object>)context.RouteData.Values).Keys.Cast<string>());
+                keys.AddRange(context.HttpContext.Request.QueryString.Keys.Cast<string>());
+                keys.AddRange(context.HttpContext.Request.Files.Keys.Cast<string>());
+                contextItems["ValueProviderKeys"] = keys;
+            }
+            return (IEnumerable<string>)contextItems["ValueProviderKeys"];
 #else
             return bindingContext.ValueProvider.Keys;
 #endif
@@ -50,13 +56,29 @@ using System.ComponentModel;
         public override object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
         {
             Type modelType = bindingContext.ModelType;
-            Type idictType = modelType.GetInterface("System.Collections.Generic.IDictionary`2");
+            Type idictType = null;
+            Type genericTypeDefinition = null;
+
+            // For collection classes, proceed as dictionary, then convert back to list.
+            if (modelType.IsGenericType) {
+                genericTypeDefinition = modelType.GetGenericTypeDefinition();
+                if (genericTypeDefinition == typeof(IDictionary<,>)) {
+                    idictType = typeof(Dictionary<,>).MakeGenericType(modelType.GetGenericArguments());
+                }
+                else if (genericTypeDefinition == typeof(IEnumerable<>) || genericTypeDefinition == typeof(ICollection<>) || genericTypeDefinition == typeof(IList<>)) {
+                    Type[] genericItemType = modelType.GetGenericArguments();
+                    Type[] genericArgs = new Type[] { typeof(int),  genericItemType[0] };
+                    idictType = typeof(Dictionary<,>).MakeGenericType(genericArgs);
+                }
+            }
+
             if (idictType != null)
             {
                 object result = null;
 
                 Type[] ga = idictType.GetGenericArguments();
                 IModelBinder valueBinder = Binders.GetBinder(ga[1]);
+                List<object> dictionaryKeys = new List<object>();
                 
                 foreach (string key in GetValueProviderKeys(controllerContext))
                 {
@@ -76,6 +98,12 @@ using System.ComponentModel;
                             continue;
                         }
 
+                        if (dictionaryKeys.Contains(dictKey))
+                        {
+                            continue;
+                        }
+                        dictionaryKeys.Add(dictKey);
+
                         ModelBindingContext innerBindingContext = new ModelBindingContext()
                         {
 #if ASPNETMVC1
@@ -92,10 +120,27 @@ using System.ComponentModel;
                         object newPropertyValue = valueBinder.BindModel(controllerContext, innerBindingContext);
 
                         if (result == null)
-                            result = CreateModel(controllerContext, bindingContext, modelType);
+                            result = CreateModel(controllerContext, bindingContext, idictType);
 
                         if (!(bool)idictType.GetMethod("ContainsKey").Invoke(result, new object[] { dictKey }))
                             idictType.GetProperty("Item").SetValue(result, newPropertyValue, new object[] { dictKey });
+                    }
+                }
+
+                if (result != null)
+                {
+                    if (genericTypeDefinition == typeof(IEnumerable<>) || genericTypeDefinition == typeof(ICollection<>) || genericTypeDefinition == typeof(IList<>))
+                    {
+                        //  Here is where we convert back to a list.
+                        IEnumerable collectionResult = (IEnumerable)idictType.GetProperty("Values").GetValue(result, null);
+                        Type listType = typeof(List<>).MakeGenericType(modelType.GetGenericArguments());
+                        object listObject = Activator.CreateInstance(listType);
+                        foreach (var item in collectionResult)
+                        {
+                            listType.GetMethod("Add").Invoke(listObject, new object[] { item });
+                        }
+
+                        return listObject;
                     }
                 }
 
